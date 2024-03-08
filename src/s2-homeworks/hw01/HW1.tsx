ffdc554cc1d5928765e6222766502f5616db4870
ivanrussui <fenomen_ny@mail.ru>
2024-03-08 13:08:58 +0300

    HW01

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -13,30 +13,43 @@ import avatar from './avatar.png'
 * 5 - сделать стили в соответствии с дизайном
 * */
 
-// нужно создать правильный тип вместо any
-export type MessageType = any
+type UserObjType = {
+    name: string
+    avatar: string
+}
+
+type MessageObjType = {
+    text: string
+    time: string
+}
+
+export type MessageType = {
+    id: number
+    user: UserObjType
+    message: MessageObjType
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'Ivan',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
-        time: '22:00', // можно менять
+        text: 'Hello, she didn’t do anything and rested all day, how are you?', // можно менять
+        time: '09:01', // можно менять
     },
 }
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Ivan', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
+        text: 'Hello, how are you, what did you do yesterday?', // можно менять
+        time: '09:00', // можно менять
     },
 }
 
--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,49 +1,42 @@
-import React from 'react'
-import s from './FriendMessage.module.css'
+import React from 'react';
+import s from './FriendMessage.module.css';
+import {MessageType} from '../HW1';
+
+type FriendMessagePropsType = {
+    message: MessageType
+}
 
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
+const FriendMessage = ({message}: FriendMessagePropsType) => {
     return (
-        <div
-            id={'hw1-friend-message-' + props.message.id}
-            className={s.friendMessage}
-        >
+        <div id={'hw1-friend-message-' + message.id}
+             className={s.friendMessage}>
             <div className={s.friendImageAndText}>
                 <img
-                    id={'hw1-friend-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
+                    id={'hw1-friend-avatar-' + message.id}
+                    src={message.user.avatar}
+                    alt={message.user.name}
                 />
                 <div className={s.friendText}>
                     <div
-                        id={'hw1-friend-name-' + props.message.id}
-                        className={s.friendName}
-                    >
-                        {/*создаёт студент*/}
-
-                        {/**/}
+                        id={'hw1-friend-name-' + message.id}
+                        className={s.friendName}>
+                        {message.id}
                     </div>
                     <pre
-                        id={'hw1-friend-text-' + props.message.id}
-                        className={s.friendMessageText}
-                    >
-                        {/*создаёт студент*/}
-
-                        {/**/}
+                        id={'hw1-friend-text-' + message.id}
+                        className={s.friendMessageText}>
+                        {message.message.text}
                     </pre>
                 </div>
             </div>
             <div
-                id={'hw1-friend-time-' + props.message.id}
-                className={s.friendTime}
-            >
-                {/*создаёт студент*/}
-
-                {/**/}
+                id={'hw1-friend-time-' + message.id}
+                className={s.friendTime}>
+                {message.message.time}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default FriendMessage
+export default FriendMessage;
