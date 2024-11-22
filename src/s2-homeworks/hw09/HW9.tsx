e7d4820273db3e6f82f7f67bdbebf8138e90f643
Ivan <fenomen_ny@mail.ru>
2024-11-22 20:29:12 +0300

    hw 9

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Clock from './Clock'
-import s2 from '../../s1-main/App.module.css'
+import React from 'react';
+import Clock from './Clock';
+import s2 from '../../s1-main/App.module.css';
 
 /*
 * 1 - в файле Clock.tsx дописать функции stop, start, onMouseEnter, onMouseLeave
@@ -15,10 +15,10 @@ const HW9 = () => {
             <div className={s2.hwTitle}>Homework #9</div>
 
             <div className={s2.hw}>
-                <Clock />
+                <Clock/>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default HW9
+export default HW9;
