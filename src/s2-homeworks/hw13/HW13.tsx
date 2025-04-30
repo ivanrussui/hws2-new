7d35b343f699f0edb91f5c8cb92938f3ae4702c7
ivanrussui <fenomen_ny@mail.ru>
2025-05-01 01:23:43 +0300

    hw 13

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -1,12 +1,12 @@
-import React, {useState} from 'react'
-import s2 from '../../s1-main/App.module.css'
-import s from './HW13.module.css'
-import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import axios from 'axios'
-import success200 from './images/200.svg'
-import error400 from './images/400.svg'
-import error500 from './images/500.svg'
-import errorUnknown from './images/error.svg'
+import React, {useState} from 'react';
+import s2 from '../../s1-main/App.module.css';
+import s from './HW13.module.css';
+import SuperButton from '../hw04/common/c2-SuperButton/SuperButton';
+import axios, {AxiosError} from 'axios';
+import success200 from './images/200.svg';
+import error400 from './images/400.svg';
+import error500 from './images/500.svg';
+import errorUnknown from './images/error.svg';
 
 /*
 * 1 - дописать функцию send
@@ -14,36 +14,64 @@ import errorUnknown from './images/error.svg'
 * 3 - сделать стили в соответствии с дизайном
 * */
 
+type ResponseType = {
+    errorText: string;
+    info: string;
+    yourBody: { success: boolean };
+    yourQuery: {};
+}
+
+type IsSendingType = string | boolean | undefined | null;
+
 const HW13 = () => {
-    const [code, setCode] = useState('')
-    const [text, setText] = useState('')
-    const [info, setInfo] = useState('')
-    const [image, setImage] = useState('')
+    const [code, setCode] = useState('');
+    const [text, setText] = useState('');
+    const [info, setInfo] = useState('');
+    const [image, setImage] = useState('');
+    const [isSending, setIsSending] = useState<IsSendingType>('');
+
 
     const send = (x?: boolean | null) => () => {
+        setIsSending(x)
+
         const url =
             x === null
                 ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
-                : 'https://samurai.it-incubator.io/api/3.0/homework/test'
+                : 'https://samurai.it-incubator.io/api/3.0/homework/test';
 
-        setCode('')
-        setImage('')
-        setText('')
-        setInfo('...loading')
+        setCode('');
+        setImage('');
+        setText('');
+        setInfo('...loading');
 
         axios
-            .post(url, {success: x})
+            .post<ResponseType>(url, {success: x})
+
             .then((res) => {
-                setCode('Код 200!')
-                setImage(success200)
+                setCode('Код 200!');
+                setImage(success200);
                 // дописать
-
+                setText(res.data.errorText);
+                setInfo(res.data.info);
             })
-            .catch((e) => {
+            .catch((e: AxiosError<ResponseType>) => {
                 // дописать
+                const code = e.response?.status === 500 ? 'Ошибка 500!' : e.response?.status === 400 ? 'Ошибка 400!' : 'Error!';
+                const image = e.response?.status === 500 ? error500 : e.response?.status === 400 ? error400 : errorUnknown;
+                const errorText = e.response?.data ? e.response.data.errorText : e.message;
+                const info = e.response?.data ? e.response.data.info : e.name;
 
+                setCode(code);
+                setImage(image);
+                setText(errorText);
+                setInfo(info);
             })
-    }
+            .finally(() => {
+                setIsSending('');
+            });
+    };
+
+    const isButtonDisabled = (value: IsSendingType) => isSending === value;
 
     return (
         <div id={'hw13'}>
@@ -56,7 +84,8 @@ const HW13 = () => {
                         onClick={send(true)}
                         xType={'secondary'}
                         // дописать
-
+                        // disabled={isSending === true}
+                        disabled={isButtonDisabled(true)}
                     >
                         Send true
                     </SuperButton>
@@ -65,7 +94,8 @@ const HW13 = () => {
                         onClick={send(false)}
                         xType={'secondary'}
                         // дописать
-
+                        // disabled={isSending === false}
+                        disabled={isButtonDisabled(false)}
                     >
                         Send false
                     </SuperButton>
@@ -74,7 +104,8 @@ const HW13 = () => {
                         onClick={send(undefined)}
                         xType={'secondary'}
                         // дописать
-
+                        // disabled={isSending === undefined}
+                        disabled={isButtonDisabled(undefined)}
                     >
                         Send undefined
                     </SuperButton>
@@ -83,7 +114,8 @@ const HW13 = () => {
                         onClick={send(null)} // имитация запроса на не корректный адрес
                         xType={'secondary'}
                         // дописать
-
+                        // disabled={isSending === null}
+                        disabled={isButtonDisabled(null)}
                     >
                         Send null
                     </SuperButton>
@@ -108,7 +140,7 @@ const HW13 = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default HW13
+export default HW13;
